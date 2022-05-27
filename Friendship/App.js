856dc27dfa4fce21a864287d0dea95c416ea04b1
Muhammed Mertoğlu <97661173+mmertoglu@users.noreply.github.com>
2022-05-27 19:06:50 +0300

    messagecard added

--- Friendship/App.js
+++ Friendship/App.js
@@ -8,7 +8,8 @@ import Friends from './src/pages/Friends/Friends';
 import {NavigationContainer} from '@react-navigation/native'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
-
+import Messages from './src/pages/messages/Messages';
+import SignScreen from './src/pages/SignScreen/SignScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,10 +17,12 @@ const App = () => {
   return(
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown:false}} >
+      <Stack.Screen name='SignScreen' component={SignScreen} />
         <Stack.Screen name='LoginScreen' component={LoginScreen} />
         <Stack.Screen name='HomeScreen' component={Home} />
         <Stack.Screen name='FriendshipRequestScreen' component={FriendshipRequest} />
         <Stack.Screen name='FriendsScreen' component={Friends} />
+        <Stack.Screen name='MessagesScreen' component={Messages} />
       </Stack.Navigator>
     </NavigationContainer>
   )
